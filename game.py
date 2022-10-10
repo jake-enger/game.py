ad6139679d0d62c4e0eb30f511b110c6d289c6e4
jake-enger <jake.enger@gmail.com>
2022-10-10 15:56:02 -0600

    Finished the guessing game

---
+++ game.py
@@ -0,0 +1,18 @@
+from random import randint
+from itertools import count
+
+playerName = (input("Lets play a number guessing game! What is your name?\n"))
+
+def number_guessing(minimum, maximum):
+    randomNum = randint(minimum, maximum)
+    for tries in count(1):
+        numPlayer = int(input("Guess a number between ({}-{}) \n".format(minimum, maximum)))
+        if numPlayer > randomNum:
+            print ("Too high, try again!")
+        elif numPlayer < randomNum:
+            print ("Too low, try again!")
+        elif numPlayer == randomNum:
+            return tries
+
+print ("Well done,", playerName, "you guessed my number in {} guesses!".format(
+    number_guessing(1, 2)))
